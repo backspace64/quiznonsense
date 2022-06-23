d3c63bc19492b9af4b937fe91627fc196443186d
backspace64 <cheezydixon@gmail.com>
2022-06-22 20:53:53 -0500

    qiuz

---
+++ project/script.js
@@ -0,0 +1,25 @@
+//List of questions
+//1Q.What does html stand for?
+//Answer: Hypertext Markup Language
+//Options: 
+//1.Hypertail Madagascar Liger
+//2.Hypertext Markup Language
+//3.Hypertext Men Langauge
+//4.hypertext markdown Langauge
+//2Q.what does css stand for?
+//Answer: Cascadinig Style Sheet
+//Options:
+//1.Cascadinig Style Sheet
+//2.Compound Serivce Soultions
+//3.Crypted Scaling Servers
+//4. Conference Sky SChool
+//3Q.what should you do before you want to push codes to the respistory?
+//Answer: Git Commit
+//1.Git Commit
+//2.Git Inint
+//3.Git Submit
+//4.Git puLL
+
+function Start() {
+    window.alert("Would you like to start the Quiz?");
+}
